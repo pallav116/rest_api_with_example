dd7b6e8a4742904010a574211ec19193be5cfb64
hamed naeemaei <h.naimaei@gmail.com>
2025-08-29 20:51:10 +0330

    Strings: Remove Seed function call

--- src/common/strings.go
+++ src/common/strings.go
@@ -6,7 +6,6 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	"time"
 	"unicode"
 
 	"github.com/naeemaei/golang-clean-web-api/config"
@@ -107,7 +106,6 @@ func GeneratePassword() string {
 
 func GenerateOtp() string {
 	cfg := config.GetConfig()
-	rand.Seed(time.Now().UnixNano())
 	min := int(math.Pow(10, float64(cfg.Otp.Digits-1)))   // 10^d-1 100000
 	max := int(math.Pow(10, float64(cfg.Otp.Digits)) - 1) // 999999 = 1000000 - 1 (10^d) -1
 
